e604824f513dd613dfce375b3cf44f2eb7f3af17
John Kelleher <84353883+JKelleher00@users.noreply.github.com>
2021-06-04 16:21:08 -0400

    Update Problem6.java

--- src/main/java/achoo/Problem6.java
+++ src/main/java/achoo/Problem6.java
@@ -1,6 +1,9 @@
 package achoo;
 import java.util.*;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 John Kelleher
+ */
 public class Problem6 {
     public static void main(String args[]){
         Scanner a = new Scanner(System.in);
